239952113c4c8fb747d4cf32db31ac3317030c6b
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-02 11:54:06 -0800

    Made base template files to use each day

---
+++ day.py
@@ -0,0 +1,14 @@
+#Template python file for each day.
+
+def part1():
+    return True
+
+def part2():
+    return True
+
+def main():
+    print('Part 1 Solution: ' + str(part1()))
+    print('Part 2 Soluction: ' + str(part2()))
+
+if __name__ == '__main__':
+    main()
