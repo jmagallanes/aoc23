3516fc667f42d65b680720271eccf78fc4199f33
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-02 11:54:54 -0800

    Added Day 3 base files

---
+++ Day 3/day3.py
@@ -0,0 +1,12 @@
+def part1():
+    return True
+
+def part2():
+    return True
+
+def main():
+    print('Part 1 Solution: ' + str(part1()))
+    print('Part 2 Soluction: ' + str(part2()))
+
+if __name__ == '__main__':
+    main()
