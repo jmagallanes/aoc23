387601f805bdaa10d4bbc51ed2e8de77d53279c5
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-02 08:47:19 -0800

    Refactored code to better modularize functions

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -2,6 +2,21 @@ redMax = 12
 greenMax = 13
 blueMax = 15
 
+def getGames():
+    games = []
+    with open('input.txt') as inputFile:
+        for line in inputFile:
+            line = line.strip()
+            game = []
+            for setGrab in (line[line.find(':')+1:]).split(';'):
+                indSet = []
+                for indGrab in setGrab.split(','):
+                    indGrab = indGrab.split()
+                    indSet.append(indGrab)
+                game.append(indSet)
+            games.append(game)
+    return games
+
 def checkMax(indGrab):
     match indGrab[1]:
         case 'red':
@@ -15,23 +30,21 @@ def checkMax(indGrab):
                 return False
     return True
 
-def part1():
+def part1(games):
     idSums = 0
-    with open('input.txt') as inputFile:
-        for line in inputFile:
-            possible = True
-            line = line.strip()
-            for setGrab in (line[line.find(':')+1:]).split(';'):
-                for indGrab in setGrab.split(','):
-                    indGrab = indGrab.split()
-                    if not checkMax(indGrab):
-                        possible = False
-            if possible:
-                idSums += int(line[line.find(' '):line.find(':')])
+    for index, game in enumerate(games):
+        possible = True
+        for setGrab in game:
+            for indGrab in setGrab:
+                if not checkMax(indGrab):
+                    possible = False
+        if possible:
+            idSums += index + 1
     return(idSums)
 
 def main():
-    print('Part 1 Solution: ' + str(part1()))
+    games = getGames()
+    print('Part 1 Solution: ' + str(part1(games)))
 
 if __name__ == '__main__':
     main()
