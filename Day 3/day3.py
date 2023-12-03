79475ae855360d9901780ca198bd07354a01dda8
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-03 07:06:40 -0800

    Added function to build out matrix from input

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,12 +1,24 @@
-def part1():
-    return True
+def buildMatrix():
+    matrix = []
+    with open('test.txt') as inputFile:
+        for line in inputFile:
+            line = line.strip()
+            row = []
+            for char in line:
+                row.append(char)
+            matrix.append(row)
+    return(matrix)
 
-def part2():
-    return True
+def part1(matrix):
+    return "TBD"
+
+def part2(matrix):
+    return "TBD"
 
 def main():
-    print('Part 1 Solution: ' + str(part1()))
-    print('Part 2 Soluction: ' + str(part2()))
+    matrix = buildMatrix()
+    print('Part 1 Solution: ' + str(part1(matrix)))
+    print('Part 2 Soluction: ' + str(part2(matrix)))
 
 if __name__ == '__main__':
     main()
