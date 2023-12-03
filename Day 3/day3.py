9d1a0dcbcae2781826b12f88ca32093208bd3429
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-03 10:18:23 -0800

    Added functions and logic to check surrounding chars for symbol

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -9,6 +9,46 @@ def buildMatrix():
             matrix.append(row)
     return(matrix)
 
+def checkForSymbol(char):
+    if char.isdigit():
+        return False
+    elif char == '.':
+        return False
+    elif char.isalpha():
+        return False
+    else:
+        return True
+
+
+def numCheck(matrix,row,colIndex,num):
+    start = colIndex - len(num)
+    foundSymbol = False
+    for i in range(0, len(num)):
+        #check left/up
+        if not start + i == 0 and not row == 0:
+            checkForSymbol(matrix[row-1][start + i-1])
+        #check up
+        if not row == 0:
+            checkForSymbol(matrix[row-1][start + i])
+        # #check up/right
+        if not start + i == len(matrix[0]) and not row == 0:
+            checkForSymbol(matrix[row-1][start + i + 1])
+        #check right
+        if not start + i == len(matrix[0]):
+            checkForSymbol(matrix[row][start + i + 1])
+        #check right/down
+        if not start + i == len(matrix[0]) and not row + 1  == len(matrix):
+            checkForSymbol(matrix[row+1][start + i + 1])
+        #check down
+        if not row + 1 == len(matrix):
+            checkForSymbol(matrix[row+1][start + i])
+        #check down/left
+        if not start + i -1 == 0 and not row +1 == len(matrix):
+            checkForSymbol(matrix[row+1][start + i - 1])
+        #check left
+        if not start + i - 1 == 0:
+            checkForSymbol(matrix[row][start + i - 1])
+
 def part1(matrix):
     for rowIndex, row in enumerate(matrix):
         tempNum = ""
@@ -16,9 +56,11 @@ def part1(matrix):
         for colIndex, char in enumerate(row):
             if char.isdigit():
                 tempNum += char
-            else:
-                if not tempNum == "":
-                    print(tempNum + " " + str(rowIndex) + ", " + str(colIndex))
+            elif not tempNum == "":
+                numCheck(matrix,rowIndex,colIndex,tempNum)
+                tempNum = ""
+            if colIndex == len(matrix[0]) - 1 and not tempNum == "":
+                numCheck(matrix,rowIndex,colIndex,tempNum)
                 tempNum = ""
 
 def part2(matrix):
