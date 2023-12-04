93f79b6ce67572c906c153e220b2f6e32f0bb7a8
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-03 21:51:19 -0800

    Wrote part1 function to return total points

--- Day 4/day.py
+++ Day 4/day.py
@@ -1,4 +1,3 @@
-#Template python file for each day.
 def getNums():
     wNums = []
     mNums = []
@@ -11,15 +10,23 @@ def getNums():
             mNums.append(nums[1][1:].split())
     return([wNums,mNums])
 
-def part1():
-    return 'TBD'
+def part1(nums):
+    totalPoints = 0
+    for index,mNums in enumerate(nums[1]):
+        points = 0
+        for mNum in mNums:
+            if mNum in nums[0][index]:
+                if points == 0: points = 1
+                else: points = points * 2
+        totalPoints += points
+    return(totalPoints)
 
 def part2():
     return 'TBD'
 
 def main():
     nums = getNums()
-    print('Part 1 Solution: ' + str(part1()))
+    print('Part 1 Solution: ' + str(part1(nums)))
     print('Part 2 Soluction: ' + str(part2()))
 
 if __name__ == '__main__':
