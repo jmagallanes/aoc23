7029fcda6d5222e261f272cfddbd6856083c1c76
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-03 21:44:06 -0800

    Wrote function to parse input

--- Day 4/day.py
+++ Day 4/day.py
@@ -1,16 +1,24 @@
 #Template python file for each day.
-def parseFile():
-    with open('input.txt') as inputFile:
+def getNums():
+    wNums = []
+    mNums = []
+    with open('test.txt') as inputFile:
         for line in inputFile:
             line = line.strip()
+            nums = line.split(':')[1]
+            nums = nums.split('|')
+            wNums.append(nums[0][:-1].split())
+            mNums.append(nums[1][1:].split())
+    return([wNums,mNums])
 
 def part1():
-    return True
+    return 'TBD'
 
 def part2():
-    return True
+    return 'TBD'
 
 def main():
+    nums = getNums()
     print('Part 1 Solution: ' + str(part1()))
     print('Part 2 Soluction: ' + str(part2()))
 
