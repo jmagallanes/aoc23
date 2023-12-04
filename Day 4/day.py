2728852dfedff57b0647b5b86519903f21ce4da5
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-03 21:25:08 -0800

    Added Day 4

---
+++ Day 4/day.py
@@ -0,0 +1,18 @@
+#Template python file for each day.
+def parseFile():
+    with open('input.txt') as inputFile:
+        for line in inputFile:
+            line = line.strip()
+
+def part1():
+    return True
+
+def part2():
+    return True
+
+def main():
+    print('Part 1 Solution: ' + str(part1()))
+    print('Part 2 Soluction: ' + str(part2()))
+
+if __name__ == '__main__':
+    main()
