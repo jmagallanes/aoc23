a5fdcece5d7e33d281c1e2970de2b0b63550d2fd
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-04 17:23:20 -0800

    Staged Day 5

---
+++ Day 5/day5.py
@@ -0,0 +1,17 @@
+def parseFile():
+    with open('input.txt') as inputFile:
+        for line in inputFile:
+            line = line.strip()
+
+def part1():
+    return 'TBD'
+
+def part2():
+    return 'TBD'
+
+def main():
+    print('Part 1 Solution: ' + str(part1()))
+    print('Part 2 Soluction: ' + str(part2()))
+
+if __name__ == '__main__':
+    main()
