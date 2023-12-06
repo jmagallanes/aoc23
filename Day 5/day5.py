027da1c9494f43b2b6f581db4c40306b747ed7fa
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-05 20:38:47 -0800

    Wrote parseFile function which returns a map of maps coresponding to all the translations

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,5 +1,7 @@
 def parseFile():
     seeds = []
+    maps = {}
+    fullMaps = {}
     with open('test.txt') as inputFile:
         for index, line in enumerate(inputFile):
             line = line.strip()
@@ -7,18 +9,35 @@ def parseFile():
             if index == 0:
                 for each in line.split(":")[1].split():
                     seeds.append(each)
-    return(seeds)
+            #Skip empty lines
+            elif line == "": continue
+            #Add new dictionary keys
+            elif line[0].isalpha():
+                maps[line.split()[0]] = []
+            #Add map lines to map
+            else:
+                numList = []
+                for num in line.split():
+                    numList.append(int(num))
+                maps[list(maps)[-1]].append(numList)
+    for each in maps:
+        fullMaps[each] = {}
+        for index in range(100):
+            fullMaps[each][index] = index
+        for indMap in maps[each]:
+            for index in range(indMap[2]):
+                fullMaps[each][indMap[1]+index] = indMap[0] + index
+    return(fullMaps)
 
-def part1():
+def part1(data):
     return 'TBD'
 
-def part2():
+def part2(data):
     return 'TBD'
 
 def main():
-    print(parseFile())
-    print('Part 1 Solution: ' + str(part1()))
-    print('Part 2 Soluction: ' + str(part2()))
+    print('Part 1 Solution: ' + str(part1(data)))
+    print('Part 2 Soluction: ' + str(part2(data)))
 
 if __name__ == '__main__':
     main()
