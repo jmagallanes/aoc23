e6c872f5fd1069f592b0496fe9c977fe9f2572ee
Jose Magallanes <jmmagallanes@gmail.com>
2023-12-05 20:51:28 -0800

    Wrote part1, which returns lowest location with the given seeds

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -8,7 +8,7 @@ def parseFile():
             #Create list of seeds
             if index == 0:
                 for each in line.split(":")[1].split():
-                    seeds.append(each)
+                    seeds.append(int(each))
             #Skip empty lines
             elif line == "": continue
             #Add new dictionary keys
@@ -27,15 +27,25 @@ def parseFile():
         for indMap in maps[each]:
             for index in range(indMap[2]):
                 fullMaps[each][indMap[1]+index] = indMap[0] + index
-    return(fullMaps)
+    return(seeds,fullMaps)
 
 def part1(data):
-    return 'TBD'
-
+    seeds = data[0]
+    fullMaps = data[1]
+    lowestSeed = 1000
+    for seed in seeds:
+        curr = seed
+        for each in fullMaps:
+            curr = fullMaps[each][curr]
+        if curr < lowestSeed:
+            lowestSeed = curr
+    return(lowestSeed)
+    
 def part2(data):
     return 'TBD'
 
 def main():
+    data = parseFile()
     print('Part 1 Solution: ' + str(part1(data)))
     print('Part 2 Soluction: ' + str(part2(data)))
 
